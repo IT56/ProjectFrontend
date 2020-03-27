734b89c1de4097ab7f577ce891f4f7d29aec2497
Ira <Irinat403@gmail.com>
2020-03-27 20:39:30 +0300

    Calendar

---
+++ src/js/script_one.js
@@ -0,0 +1,26 @@
+// // Календарь
+
+
+//  var elems = document.getElementsByClassName('.datepicker-here');
+//  var elems2 = document.getElementsByClassName('.fas')
+
+// $('.elems').click(function(){
+//     $('.elems2').hide();
+// });
+
+
+
+
+
+// $('.datepicker-here').datepicker({
+//   onSelect: function (fd, d, picker) { 
+//     $(".datepicker-here").val(fd.split("-")[0]);
+//     $("#dat").val(fd.split("-")[1]);
+//   }
+    
+// })
+
+// $('.datepicker-here').datepicker({
+//     clearButton: true
+// });
+
